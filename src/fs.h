e101e282a8609483f5e198c245b8fe750a46c798
ozekiah <ozekiah@tuta.com>
2025-01-14 18:50:00 +0100

    create inode dir for snapshot

--- src/fs.h
+++ src/fs.h
@@ -4,6 +4,7 @@
 #include <archive.h>
 
 int create_tar_xz(const char *src, const char *dst);
+long int get_dir_inode(const char *dir_path);
 int path_exists(const char *path);
 
 #endif
--- src/snapshot.c
+++ src/snapshot.c
@@ -6,12 +6,20 @@
 
 int create_snapshot()
 {
-        char *source_directory = opts.path[0];
+        char *source_dir = opts.path[0];
+        long int source_inode = get_dir_inode(source_dir);
+
+        char destination_dir[4096];
+        snprintf(destination_dir, sizeof(destination_dir), "%s/%ld", config.path, source_inode);
+        if (mkdir(destination_dir, 0777) == -1) {
+                perror("mkdir");
+                return 1;
+        }
 
         char destination_file[4096];
-        snprintf(destination_file, sizeof(destination_file), "%s/test1.tar.xz", config.path);
+        snprintf(destination_file, sizeof(destination_file), "%s/testfile.tar.xz", destination_dir);
 
-        int result = create_tar_xz(source_directory, destination_file);
+        int result = create_tar_xz(source_dir, destination_file);
         if (result != 0) {
                 fprintf(stderr, "Error creating tar.xz archive\n");
                 return 1;
